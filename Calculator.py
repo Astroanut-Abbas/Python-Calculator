f5f02a4981ba00194af0ae45b327c24616b73f08
Astroanut-Abbas <135329350+Astroanut-Abbas@users.noreply.github.com>
2023-11-26 13:57:48 +0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,13 @@
-
+# Taking input from user
+num1 = float(input("Enter Your first No.: "))
+opr = (input("+,-,*,/"))
+num2 = float(input("Enter Your Second No.: "))
+# Applying Conditions
+if (opr == +):
+  print(num1 + num2)
+elif (opr == -):
+  print(num1 - num2)
+elif (opr == *):
+  print(num1 * num2)
+else :
+  print("Invalid Input")
